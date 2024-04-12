4259d11505517f090edfcc72887be38a783e5179
Kristo Lippur <kristo@elnino.tech>
2024-04-12 18:44:08 +0200

    Add more info to Readme and make EF Core and GraphQL configuration easier

---
+++ StrictId.EFCore/EfCoreExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StrictId.EFCore.Conventions;
+using StrictId.EFCore.ValueConverters;
+using StrictId.EFCore.ValueGenerators;
+
+namespace StrictId.EFCore;
+
+public static class EfCoreExtensions
+{
+	public static ModelConfigurationBuilder ConfigureStrictId (this ModelConfigurationBuilder builder)
+	{
+		builder.Properties<Id>()
+			.HaveConversion<IdToStringConverter>()
+			.HaveMaxLength(26)
+			.AreFixedLength();
+		
+		builder.Properties(typeof(Id<>))
+			.HaveConversion(typeof(IdToStringConverter<>))
+			.HaveMaxLength(26)
+			.AreFixedLength();
+		
+		builder.Conventions.Add(_ => new IdConvention());
+
+		return builder;
+	}
+
+	public static PropertyBuilder<Id> HasIdValueGenerator<T> (this PropertyBuilder<Id> builder)
+	{
+		builder
+			.HasValueGenerator<IdValueGenerator>()
+			.HasValueGeneratorFactory<IdValueGeneratorFactory>();
+
+		return builder;
+	}
+	
+	public static PropertyBuilder<Id<T>> HasStrictIdValueGenerator<T> (this PropertyBuilder<Id<T>> builder)
+	{
+		builder
+			.HasValueGenerator<IdTypedValueGenerator<T>>()
+			.HasValueGeneratorFactory<IdTypedValueGeneratorFactory<T>>();
+
+		return builder;
+	}
+}
---
+++ StrictId.HotChocolate/GraphQlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StrictId.HotChocolate;
+
+public static class GraphQlExtensions
+{
+	/// <summary>
+	/// Add support for strict ID types Id<T> and Id
+	/// </summary>
+	/// <param name="builder"></param>
+	/// <returns></returns>
+	public static IRequestExecutorBuilder AddStrictId (this IRequestExecutorBuilder builder) =>
+		builder
+			.AddType(new IdScalar())
+			.TryAddTypeInterceptor<IdTypeInterceptor>(); // Why is this method called "TryAdd" if it just adds without trying...?
+}
