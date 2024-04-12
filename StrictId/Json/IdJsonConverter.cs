31011120f467afa8bf185a669a72204380f6a53f
Lippur <k@lippur.com>
2024-04-12 17:17:47 +0200

    Add correct JSON converter

--- StrictId/Json/IdJsonConverter.cs
+++ StrictId/Json/IdJsonConverter.cs
@@ -2,7 +2,7 @@
 using System.Text.Json.Serialization;
 using Cysharp.Serialization.Json;
 
-namespace Corpo.Core.Framework.Database.Identifiers;
+namespace StrictId.Json;
 
 public class IdJsonConverter : JsonConverter<Id>
 {
@@ -18,4 +18,26 @@ public class IdJsonConverter : JsonConverter<Id>
 
 	public override void Write (Utf8JsonWriter writer, Id value, JsonSerializerOptions options) =>
 		_ulidJsonConverter.Write(writer, value.Value, options);
+}
+
+public class IdTypedJsonConverterFactory : JsonConverterFactory
+{
+	public override bool CanConvert (Type typeToConvert) =>
+		typeToConvert.IsGenericType && typeToConvert.GetGenericTypeDefinition() == typeof(Id<>);
+
+	public override JsonConverter? CreateConverter (Type typeToConvert, JsonSerializerOptions options) =>
+		(JsonConverter?)Activator.CreateInstance(
+			typeof(IdTypedJsonConverter<>).MakeGenericType(typeToConvert.GetGenericArguments().First())
+		);
+
+	private class IdTypedJsonConverter<T> : JsonConverter<Id<T>>
+	{
+		private readonly UlidJsonConverter _ulidJsonConverter = new();
+
+		public override Id<T> Read (ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+			new(_ulidJsonConverter.Read(ref reader, typeToConvert, options));
+
+		public override void Write (Utf8JsonWriter writer, Id<T> value, JsonSerializerOptions options) =>
+			_ulidJsonConverter.Write(writer, value.Value, options);
+	}
 }
